0f4e9d63dd313d0c75f761eda4b6e0cf7a10c5fc
steve_D <scd346@nyu.edu>
2017-06-09 16:19:55 +0200

    Updated app.js, pug files.  Blog working.  Code still messy.  Cleaning up this weekend.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+//CHECK: do i need pg?  do i need morgan?
+
+
 //required packages for the app//
 const express = require('express'),
     // morgan = require('morgan'), //middle ware for console.log's in the terminal (debugging)
